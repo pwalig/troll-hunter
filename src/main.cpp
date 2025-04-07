cc32eed15435bf7df68fb1d430d6854903e94bba
mm-102 <maciej.kowalski@student.put.poznan.pl>
2025-04-07 16:41:57 +0200

    Add msg MPI_Datatype & finish main loop  - also fix various errors eg. not initialized vectors

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,8 @@ class Message {
         releaseData release;
     };
     enum class Type { REQ, ACK, RELEASE };
-    Type type;
+    // Type type;
+    unsigned short type;
     int logicalTimestamp;
     Data data;
 
@@ -29,7 +30,7 @@ class Message {
 
     static Message req(int logiTime, int source) {
         Message res;
-        res.type = Type::REQ;
+        res.type = static_cast<unsigned short>(Type::REQ);
         res.logicalTimestamp = logiTime;
         res.data.rank = source;
         return res;
@@ -37,7 +38,7 @@ class Message {
 
     static Message ack(int logiTime, int source) {
         Message res;
-        res.type = Type::ACK;
+        res.type = static_cast<unsigned short>(Type::ACK);
         res.logicalTimestamp = logiTime;
         res.data.rank = source;
         return res;
@@ -45,18 +46,45 @@ class Message {
 
     static Message release(int logiTime, int city) {
         Message res;
-        res.type = Type::ACK;
+        res.type = static_cast<unsigned short>(Type::RELEASE);
         res.logicalTimestamp = logiTime;
         res.data.release.city = city;
         res.data.release.timestamp = 
         std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch() + std::chrono::milliseconds{1000}).count();
         return res;
     }
+
+    static MPI_Datatype createMpiType(){
+        Message dummy_message;
+        MPI_Aint base_addr;
+        MPI_Aint displacements[4];
+
+        MPI_Get_address(&dummy_message, &base_addr);
+        MPI_Get_address(&dummy_message.type, &displacements[0]);
+        MPI_Get_address(&dummy_message.logicalTimestamp, &displacements[1]);
+        MPI_Get_address(&dummy_message.data.rank, &displacements[2]);
+        MPI_Get_address(&dummy_message.data.release.timestamp, &displacements[3]);
+        displacements[0] = MPI_Aint_diff(displacements[0], base_addr);
+        displacements[1] = MPI_Aint_diff(displacements[1], base_addr);
+        displacements[2] = MPI_Aint_diff(displacements[2], base_addr);
+        displacements[3] = MPI_Aint_diff(displacements[3], base_addr);
+
+        MPI_Datatype types[4] = {MPI_UNSIGNED_SHORT, MPI_INT, MPI_INT, MPI_LONG_LONG};
+        int lengths[4] = {1,1,1,1};
+
+        MPI_Datatype mpiType;
+
+        MPI_Type_create_struct(4, lengths, displacements, types, &mpiType);
+        MPI_Type_commit(&mpiType);
+
+        return mpiType;
+    }
 };
 
 class TrollHunter {
 public:
 
+    const MPI_Datatype msgType;
     std::deque<std::pair<int, int>> requestQueue;
     std::vector<unsigned int> releaseCounts;
     std::vector<long long> releaseTimestamp;
@@ -70,24 +98,32 @@ public:
 
     std::mt19937 generator;
 
-    TrollHunter(int Rank, int Size) : rank(Rank), size(Size), generator(std::random_device()()) {}
+    TrollHunter(int Rank, int Size, const MPI_Datatype msgType) : msgType(msgType), rank(Rank), size(Size), generator(std::random_device()()), releaseCounts(Size), releaseTimestamp(Size) {}
 
-    void send(Message msg, int dest=-1) {
+    void send(const Message msg, int dest=-1) {
         if (dest < 0) {
             for (int i = 0; i < size; ++i)
-                MPI_Send(&msg, sizeof(Message), MPI_BYTE, i, MPI_ANY_TAG, MPI_COMM_WORLD);
+                MPI_Send(&msg, 1, msgType, i, 0, MPI_COMM_WORLD);
         }
         else {
             assert(dest < size);
-            MPI_Send(&msg, sizeof(Message), MPI_BYTE, dest, MPI_ANY_TAG, MPI_COMM_WORLD);
+            MPI_Send(&msg, 1, msgType, dest, 0, MPI_COMM_WORLD);
         }
     }
 
-    void insertRequest(int logicalTimestamp, int processId) {
-        size_t i = requestQueue.size() - 1;
-        while (i > 0 && requestQueue[i].first > logicalTimestamp) --i;
-        while (i > 0 && requestQueue[i].first == logicalTimestamp && requestQueue[i].second > processId) --i;
-        requestQueue.insert(requestQueue.begin() + i, {logicalTimestamp, processId});
+    // void insertRequest(int logicalTimestamp, int processId) {
+    //     size_t i = requestQueue.size() - 1;
+    //     while (i > 0 && requestQueue[i].first > logicalTimestamp) --i;
+    //     while (i > 0 && requestQueue[i].first == logicalTimestamp && requestQueue[i].second > processId) --i;
+    //     requestQueue.insert(requestQueue.begin() + i, {logicalTimestamp, processId});
+    // }
+
+    void insertRequest(int logicalTimestamp, int processId){
+        auto it = std::find_if(requestQueue.rbegin(), requestQueue.rend(), 
+        [&](const std::pair<int, int> pair) -> bool{
+            return pair.first < logicalTimestamp || (pair.first == logicalTimestamp && pair.second < processId);
+        });
+        requestQueue.insert(it.base(), {logicalTimestamp, processId});
     }
 
     // size_t getOwnRequestId() {
@@ -102,7 +138,7 @@ public:
             return pair.second == rank;
         });
 
-        return requestQueue.size() - std::distance(requestQueue.rbegin(),it);
+        return std::distance(requestQueue.begin(),it.base()) - 1;
     }
 
 
@@ -110,10 +146,10 @@ public:
         MPI_Status status;
         Message msg;
         while (true) {
-            MPI_Recv(&msg, sizeof(Message), MPI_BYTE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            MPI_Recv(&msg, 1, msgType, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
             std::lock_guard<std::mutex> lock(Mutex);
             logicalClock = std::max(logicalClock, msg.logicalTimestamp) + 1;
-            switch (msg.type)
+            switch (static_cast<Message::Type>(msg.type))
             {
             case Message::Type::REQ:
                 insertRequest(msg.logicalTimestamp, msg.data.rank);
@@ -138,31 +174,35 @@ public:
 
     void mainLoop() {
         while (true) {
-            std::uniform_real_distribution distrib(1.0, 10.0);
-            std::this_thread::sleep_for(std::chrono::duration<double>(distrib(generator)));
+            std::uniform_real_distribution distrib(1.0, 2.0);
+            // std::this_thread::sleep_for(std::chrono::duration<double>(distrib(generator)));
             
             std::unique_lock<std::mutex> lock(Mutex);
             logicalClock++;
-            insertRequest(logicalClock, rank);
+
+            // dostajemy req od samych siebie więc nie dodajemy requestu tutaj
+            // insertRequest(logicalClock, rank);
+
             ackCount = 0;
             send(Message::req(logicalClock, rank));
 
-            ackCv.wait(lock, [&] {return ackCount >= size - 1;});
-            // all ack recieved
+            printf("[%d] chcę wejść do miasta\n", rank);
+            ackCv.wait(lock, [&] {return ackCount >= size;}); // dostajemy 1 ack od samych siebie
+
             size_t index = getOwnRequestId();
             int city = index % size;
             int releasesRequired = index / size;
-            
+            printf("[%d] zebrałem ack, idx: %ld, chcę wejść do %d, potrzebuję jeszcze %d releasów\n", rank, index, city, releasesRequired - releaseCounts[city]);
+
             relCv.wait(lock, [&] {return releaseCounts[city] >= releasesRequired;});
 
-            std::chrono::system_clock::time_point tp{std::chrono::milliseconds{releaseTimestamp[city]}};
+            // std::chrono::system_clock::time_point tp{std::chrono::milliseconds{releaseTimestamp[city]}};
             lock.unlock();
-            // unlock before waiting
-            std::this_thread::sleep_until(tp);
+            // std::this_thread::sleep_until(tp);
             
-            printf("[%d] wchodzę do miasta %d", rank, city);
-            std::this_thread::sleep_for(std::chrono::duration<double>(distrib(generator))); // time inside city
-            printf("[%d] wychodzę z miasta %d", rank, city);
+            printf("[%d] wchodzę do miasta %d\n", rank, city);
+            // std::this_thread::sleep_for(std::chrono::duration<double>(distrib(generator))); // czas w mieście
+            printf("[%d] wychodzę z miasta %d\n", rank, city);
 
             lock.lock();
             send(Message::release(logicalClock, city));
@@ -189,8 +229,11 @@ int main(int argc, char **argv)
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     printf("%d / %d\n", rank, size);
 
+    // create type for msg
+    MPI_Datatype msgType = Message::createMpiType();
+
     // init troll hunter
-    TrollHunter th(rank, size);
+    TrollHunter th(rank, size, msgType);
 
     // run troll hunter
     std::thread listenerThread(std::bind(&TrollHunter::listener, &th));
